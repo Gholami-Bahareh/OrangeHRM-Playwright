c2e86a4fc8a39f2869e49dc9dbe7ea577782ea5a
Gholami-Bahareh <baharehgholamii86@gmail.com>
2026-02-12 22:09:13 +0100

    dashboard tests

--- tests/dashboard.spec.js
+++ tests/dashboard.spec.js
@@ -3,11 +3,21 @@ const {LoginPage} = require('../page-objects/LoginPage');
 const {DashboardPage} = require('../page-objects/DashboardPage');
 
 
-test.only('Successful login redirects user to dashboard',async ({page}) => {
+test('Successful login redirects user to dashboard',async ({page}) => {
     const loginPage = new LoginPage(page);
     await loginPage.goto();
     await loginPage.login('Admin', 'admin123');
     const dashboardPage = new DashboardPage(page);
     await expect(page).toHaveURL('/web/index.php/dashboard/index');
     await expect(dashboardPage.accountDropdown).toBeVisible();  
+});
+
+test.only('Successful logout redirects user to login page', async ({page}) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login('Admin', 'admin123');
+    const dashboardPage = new DashboardPage(page);
+    await expect(dashboardPage.accountDropdown).toBeVisible();
+    await dashboardPage.logOut();
+    await expect(page).toHaveURL('/web/index.php/auth/login');
 });
