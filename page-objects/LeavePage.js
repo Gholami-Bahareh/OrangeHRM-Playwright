360f036b2b309d30ab4fad5dba9f5dbc3cda02d0
Gholami-Bahareh <baharehgholamii86@gmail.com>
2026-02-20 08:44:54 +0100

    dashboard test

---
+++ page-objects/LeavePage.js
@@ -0,0 +1,15 @@
+class LeavePage {
+    constructor(page) {
+        this.page = page;
+
+        //Locators
+        this.leaveBreadcrump = page.locator('.oxd-topbar-header-breadcrumb');
+    }
+
+    //Methods / Functions
+
+
+
+}
+
+module.exports = { LeavePage };
--- tests/dashboard.spec.js
+++ tests/dashboard.spec.js
@@ -2,12 +2,12 @@ const {expect} = require('@playwright/test');
 const {test} = require('../tests/fixtures');
 const {LoginPage} = require('../page-objects/LoginPage');
 const {DashboardPage} = require('../page-objects/DashboardPage');
+const {LeavePage} = require('../page-objects/LeavePage');
 
 
-test.only('Successful login redirects user to dashboard',async ({authenticatedPage}) => {
+test('Successful login redirects user to dashboard',async ({authenticatedPage}) => {
         
     const dashboardPage = new DashboardPage(authenticatedPage);
-
 
     await expect(authenticatedPage).toHaveURL('/web/index.php/dashboard/index');
     await expect(dashboardPage.accountDropdown).toBeVisible();  
@@ -20,9 +20,6 @@ test.only('Successful login redirects user to dashboard',async ({authenticatedPa
     expect(count).toBeGreaterThan(0);
 
     await expect(dashboardPage.interactiveElementsInQuickLaunchWidget.first()).toBeEnabled();
-
-    
-    
 });
 
 test('Successful logout redirects user to login page', async ({page}) => {
@@ -33,4 +30,19 @@ test('Successful logout redirects user to login page', async ({page}) => {
     await expect(dashboardPage.accountDropdown).toBeVisible();
     await dashboardPage.logOut();
     await expect(page).toHaveURL('/web/index.php/auth/login');
+});
+
+test.only('click on Leave',async ({authenticatedPage}) => {
+        
+    const dashboardPage = new DashboardPage(authenticatedPage);
+    const leavePage = new LeavePage(authenticatedPage);
+
+    await expect(dashboardPage.quickLaunchWidget).toBeVisible();
+    await dashboardPage.interactiveElementsInQuickLaunchWidget.first().waitFor({ state: 'visible' });
+    await dashboardPage.interactiveElementsInQuickLaunchWidget.first().click();
+
+    
+    await expect(authenticatedPage).toHaveURL('web/index.php/leave/assignLeave');
+    await expect(leavePage.leaveBreadcrump).toBeVisible();
+  
 });
